eb914bbdcff5221a73a9e0215f89819d6d085076
Paulina200R <paulinaramirezmorales12@gmail.com>
2024-05-23 09:52:51 -0500

    Base de datos lista

--- app/config/mysql.db.js
+++ app/config/mysql.db.js
@@ -1,9 +1,9 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-    host: process.env.MYAQL_HOST || "localhost",
-    port: process.env.MYAQL_PORT || 3306,
-    user: process.env.MYAQL_USER || "root",
-    password: process.env.MYAQL_PASSWORD || "",
-    database: process.env.MYAQL_DATABASE || "tienda",
+    host: process.env.MYSQL_HOST || "localhost",
+    port: process.env.MYSQL_PORT || 3306,
+    user: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_PASSWORD || "",
+    database: process.env.MYSQL_DATABASE || "tienda",
 })
--- app/controllers/controller.factura.js
+++ app/controllers/controller.factura.js
@@ -1,49 +1,49 @@
 import pool from "../config/mysql.db";
 
-export const mostrarFactura = async (req,res)=>{
+export const mostrarFactura = async(req,res)=>{
     const id = req.params['id'];
     try {
-        const respuesta = await pool.query(`CALL SP_MOSTRAR_FACTURA(${id});`)
+        const respuesta = await pool.query(`CALL SP_MOSTRAR_FACTURA(${id});`);
         res.json({"respuesta":respuesta});
     } catch (error) {
         res.json({"error":error});
     }
 };
-export const listarFactura = async (req,res)=>{
+export const listarFactura = async(req,res)=>{
     try {
-        const respuesta = await pool.query(`CALL SP_LISTAR_FACTURA();`)
+        const respuesta = await pool.query(`CALL SP_LISTAR_FACTURA();`);
         res.json({"respuesta":respuesta});
     } catch (error) {
         res.json({"error":error});
     }
 };
-export const crearFactura = async (req,res)=>{
+export const crearFactura = async(req,res)=>{
     const producto = req.body.producto;
     const precio = req.body.precio;
     const fecha = req.body.fecha;
     try {
-        const respuesta = await pool.query(`CALL SP_CREAR_FACTURA('${producto}','${precio}','${fecha}');`)
+        const respuesta = await pool.query(`CALL SP_CREAR_FACTURA('${producto}','${precio}','${fecha}');`);
         res.json({"respuesta":respuesta});
     } catch (error) {
         res.json({"error":error});
     }
 };
-export const modificarFactura = async (req,res)=>{
+export const modificarFactura = async(req,res)=>{
     const producto = req.body.producto;
     const precio = req.body.precio;
     const fecha = req.body.fecha;
     const id = req.body.id;
     try {
-        const respuesta = await pool.query(`CALL SP_MODIFICAR_FACTURA('${producto}','${precio}','${fecha}','${id}');`)
+        const respuesta = await pool.query(`CALL SP_MODIFICAR_FACTURA('${producto}','${precio}','${fecha}','${id}');`);
         res.json({"respuesta":respuesta});
     } catch (error) {
         res.json({"error":error});
     }
 };
-export const eliminarFactura = async (req,res)=>{
+export const eliminarFactura = async(req,res)=>{
     const id = req.body.id;
     try {
-        const respuesta = await pool.query(`CALL SP_ELIMINAR_FACTURA(${id});`)
+        const respuesta = await pool.query(`CALL SP_ELIMINAR_FACTURA(${id});`);
         res.json({"respuesta":respuesta});
     } catch (error) {
         res.json({"error":error});
